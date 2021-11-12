6807fe3e58428f0d02ef49b00cf568feb80c887a
Antonio King <antonio.j.king@gmail.com>
2021-11-11 22:41:54 -0700

    Reformat customer does not exist method in error serializer

--- app/serilaizers/error_serializer.rb
+++ app/serilaizers/error_serializer.rb
@@ -1,8 +1,10 @@
 class ErrorSerializer
   def self.customer_does_not_exist
     {
-      status: 404,
-      message: "the specified customer does not exist"
+      error: {
+        status: 404,
+        message: "the specified customer does not exist"
+      }
     }
   end
 end
